b5392c55efc22fa524fd98711a3a6df948f3d403
philipp <philsippl@gmail.com>
2017-11-22 21:07:20 +0100

    feature browser

--- sample.R
+++ sample.R
@@ -1,5 +1,5 @@
 library("jsonlite", lib.loc="/usr/local/lib/R/3.4/site-library")
-data = readLines("http://localhost:5000/aggregation")
+data = readLines("http://localhost:5000/aggregation/5a12cb75b1c0a62a828836ab")
 x = fromJSON(data)
 tmp = x$observations[[1]][[6]][,c("attribute", "value", "timestamp")]
 #reshape(x$observations[[1]][[6]], idvar="attribute", direction = "wide")
@@ -14,3 +14,6 @@ lapply(x$attribute, mean)
 
 dput(x, file="~/Downloads/x")
 dget(file="~/Downloads/x")
+
+tbl = read.csv(url("http://localhost:5000/aggregation/5a12d404b1c0a62e559bb5eb?aggregation_type=oldest&output_type=csv"))
+tbl
--- src/resources/featuresResource.py
+++ src/resources/featuresResource.py
@@ -7,13 +7,33 @@ class Features(Resource):
         super(Features, self).__init__()
 
     def get(self, crawler_id):
-        mongorequest = [
+
+        mongorequest1 = [
             {"$unwind": "$observations"},
             {"$group": {"_id": {"attribute": "$observations.attribute", "patient_id": "$_id"}, 
                 "attribute": {"$first": "$$CURRENT.observations.attribute"}, 
+                "value": {"$first": "$$CURRENT.observations.value"}, 
                 "meta": {"$first": "$$CURRENT.observations.meta"}}
             },
-            {"$group": {"_id": "$_id.patient_id", "attributes": {"$push": {"attribute": "$$CURRENT.attribute", "meta": "$$CURRENT.meta"}}}}
+            {"$group": {"_id": "$_id.patient_id", "attributes": {"$push": {"attribute": "$$CURRENT.attribute", "meta": "$$CURRENT.meta", "value": "$$CURRENT.value"}}}}
         ]
 
-        return list(mongodbConnection.get_db()[crawler_id].aggregate(mongorequest))
+        mongorequest2 = [
+            {"$unwind": "$observations"},
+            {"$group": {"_id": {"attribute": "$observations.attribute"}, 
+                "attribute": {"$first": "$$CURRENT.observations"}}},
+            {"$project": {"_id": 0, "attribute": 1}}
+        ]
+
+        features = {}
+
+        for attribute in list(mongodbConnection.get_db()[crawler_id].aggregate(mongorequest2)):
+            features[attribute["attribute"]["attribute"]] = {"counter": 0, "meta": attribute["attribute"]["meta"]}
+
+        for patient in list(mongodbConnection.get_db()[crawler_id].aggregate(mongorequest1)):
+            for attribute in patient["attributes"]:
+                if attribute["value"] is not None:
+                    features[attribute["attribute"]]["counter"] += 1
+
+
+        return features
