2884379d667bcf9b06c587ae5dd1fbd09c63b8e5
Thorsten Schwachhofer <thorsten.schwachhofer@fau.de>
2017-11-23 18:51:39 +0100

    switch resources for crawler

--- src/api.py
+++ src/api.py
@@ -17,8 +17,8 @@ CORS(app) # this will allow cross-origin requests; needed for http://petstore.sw
 api = Api(app, add_api_spec_resource=True, api_version='0.0', api_spec_url='/api/swagger') # Wrap the Api and add /api/swagger endpoint
 
 api.add_resource(swaggerResource.Swagger, '/swagger', endpoint='swaggerhtml')
-api.add_resource(crawlerResource.CrawlerJob, '/crawler/jobs', endpoint='jobs')
-api.add_resource(crawlerResource.CrawlerJobs, '/crawler/jobs/<crawler_id>', endpoint='job')
+api.add_resource(crawlerResource.CrawlerJobs, '/crawler/jobs', endpoint='jobs')
+api.add_resource(crawlerResource.CrawlerJob, '/crawler/jobs/<crawler_id>', endpoint='job')
 api.add_resource(aggregationResource.Aggregation, '/aggregation/<crawler_id>', endpoint='aggregation')
 api.add_resource(featuresResource.FeaturesBrowser, '/features/browser/<crawler_id>', endpoint='browser')
 api.add_resource(featuresResource.FeaturesSet, '/features/sets/<set_id>', endpoint='set')
