704a941f22348fbcbdcdadc188d5d51947e28270
Thorsten Schwachhofer <thorsten.schwachhofer@fau.de>
2017-12-04 15:26:59 +0100

    small cleanup

--- src/api.py
+++ src/api.py
@@ -1,30 +1,27 @@
 from flask import Flask
 from flask_restful_swagger_2 import Api
 from flask_cors import CORS
-from resources import crawlerResource
-from resources import aggregationResource
-from resources import featuresResource
-from resources import swaggerResource
 from lib import crawlerTask
+from resources.crawlerResource import Crawler, CrawlerJob, CrawlerJobs 
+from resources.aggregationResource import Aggregation
+from resources.featuresResource import FeaturesBrowser, FeaturesSet, FeaturesSets
+from resources.swaggerResource import Swagger
 import configuration
 import os
 
 app = Flask(__name__)
-
-#TODO: is this ok?
 CORS(app) # this will allow cross-origin requests; needed for http://petstore.swagger.io in swaggerResource to access whole api output
-
 api = Api(app, add_api_spec_resource=True, api_version='0.0', api_spec_url='/api/swagger') # Wrap the Api and add /api/swagger endpoint
 
-api.add_resource(swaggerResource.Swagger, '/swagger', endpoint='swaggerhtml')
-api.add_resource(crawlerResource.Crawler, '/crawler', endpoint='crawler')
-api.add_resource(crawlerResource.CrawlerJobs, '/crawler/jobs', endpoint='jobs')
-api.add_resource(crawlerResource.CrawlerJob, '/crawler/jobs/<crawler_id>', endpoint='job')
-api.add_resource(aggregationResource.Aggregation, '/aggregation/<crawler_id>', endpoint='aggregation')
-api.add_resource(featuresResource.FeaturesBrowser, '/features/browser/<crawler_id>', endpoint='browser')
-api.add_resource(featuresResource.FeaturesSet, '/features/sets/<set_id>', endpoint='set')
-api.add_resource(featuresResource.FeaturesSets, '/features/sets', endpoint='sets')
 
+api.add_resource(Swagger, '/swagger', endpoint='swaggerhtml')
+api.add_resource(Crawler, '/crawler', endpoint='crawler')
+api.add_resource(CrawlerJobs, '/crawler/jobs', endpoint='jobs')
+api.add_resource(CrawlerJob, '/crawler/jobs/<crawler_id>', endpoint='job')
+api.add_resource(Aggregation, '/aggregation/<crawler_id>', endpoint='aggregation')
+api.add_resource(FeaturesBrowser, '/features/browser/<crawler_id>', endpoint='browser')
+api.add_resource(FeaturesSet, '/features/sets/<set_id>', endpoint='set')
+api.add_resource(FeaturesSets, '/features/sets', endpoint='sets')
 
 
 if(os.environ.get("WERKZEUG_RUN_MAIN") == "true"):
