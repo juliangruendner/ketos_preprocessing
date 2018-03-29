c2630342acb6e2d0f332745a02152a5fa7f5f37c
Thorsten Schwachhofer <thorsten.schwachhofer@fau.de>
2018-03-29 16:57:59 +0200

    swagger docs for aggregationResource

--- src/resources/aggregationResource.py
+++ src/resources/aggregationResource.py
@@ -1,4 +1,5 @@
-from flask_restful import Resource, Api, reqparse, abort
+from flask_restful import reqparse, abort
+from flask_restful_swagger_2 import Api, swagger, Resource
 from lib import mongodbConnection
 import requests
 import configuration
@@ -11,18 +12,55 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+allowedAggregationTypes = ["all", "latest", "oldest"]
+supportedOutputTypes = ["csv", "json"]
+
+AGGREGATION_TYPE_STR = 'Allowed types: ' + str(allowedAggregationTypes)
+OUTPUT_TYPE_STR = 'Supported types: leave empty for raw or use ' + str(supportedOutputTypes)
+
+parser = reqparse.RequestParser()
+parser.add_argument('aggregation_type', type = str, required = True, help = AGGREGATION_TYPE_STR, location = 'args')
+parser.add_argument('output_type', type = str, location = 'args')
+
+
 class Aggregation(Resource):
-    allowedAggregationTypes = ["all", "latest", "oldest"]
 
     def __init__(self):
-        self.parser = reqparse.RequestParser()
-        self.parser.add_argument('aggregation_type', type = str, required = True, help = 'No aggregation type provided', location = 'args')
-        self.parser.add_argument('output_type', type = str, help = 'No output type provided', location = 'args')
-
         super(Aggregation, self).__init__()
 
+    @swagger.doc({
+        "description": "Aggregate data and output as specified type.",
+        "tags": ["aggregation"],
+        "parameters": [
+            {
+                "name": "crawler_id",
+                "in": "path",
+                "type": "string",
+                "description": "The ID of the crawler whose crawled data should be aggregated.",
+                "required": True
+            },
+            {
+                "name": "aggregation_type",
+                "in": "query",
+                "type": "string",
+                "description": AGGREGATION_TYPE_STR,
+                "required": True
+            },
+            {
+                "name": "output_type",
+                "in": "query",
+                "type": "string",
+                "description": OUTPUT_TYPE_STR,
+            }
+        ],
+        "responses": {
+            "200": {
+                "description": "Retrieved a json with the inserted/updated Resource."
+            }
+        }
+    })
     def get(self, crawler_id):
-        args = self.parser.parse_args()
+        args = parser.parse_args()
         aggregation_type = args["aggregation_type"]
         output_type = args["output_type"]
 
@@ -30,7 +68,7 @@ class Aggregation(Resource):
         if crawlerJob["status"] != "finished":
             return "Crawler Job did not finish yet", 404
 
-        if aggregation_type.lower() not in self.allowedAggregationTypes:
+        if aggregation_type.lower() not in allowedAggregationTypes:
             return "Wrong aggregation type provided: " + aggregation_type, 400
         
         if aggregation_type.lower() == "all" and output_type == "csv":
